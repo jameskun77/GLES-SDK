72183cb4dbf5d8e8de9404b2ee9f962af456c297
jameskun77 <wangchao104999@126.com>
2017-11-27 11:59:20 +0800

    -delete old code

--- app/src/main/java/com/example/airhockey/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.airhockey;
-
-/**
- * Created by Jameskun on 2017/10/26.
- */
-
-public class Constants {
-    public static final int BYTES_PER_FLOAT = 4;
-}
--- app/src/main/java/com/example/airhockey/util/LoggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.airhockey.util;
-
-/**
- * Created by Jameskun on 2017/10/25.
- */
-
-public class LoggerConfig {
-    public static final boolean ON = true;
-}
